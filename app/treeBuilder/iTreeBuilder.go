18b3255322f495c4effbd98ca75a06e9969f824a
Yurij Rubakha <yurij.osbb@gmail.com>
2021-08-03 16:45:25 +0300

    interface ITreeBuilder extended

--- app/treeBuilder/iTreeBuilder.go
+++ app/treeBuilder/iTreeBuilder.go
@@ -1,1 +1,8 @@
 package treeBuilder
+
+import "FileSystemTree4.mod/FileSystemTree4/domain/folderTree"
+
+type ITreeBuilder interface {
+	ScanRecurrent(f *folderTree.TreeFolderInfo)
+	Scan(f *folderTree.TreeFolderInfo) error
+}
--- domain/folderTree/folderTree.go
+++ domain/folderTree/folderTree.go
@@ -1,1 +1,47 @@
-package domain
+package folderTree
+
+type TreeFolderInfo struct {
+	Parent     *TreeFolderInfo
+	SubFolders []*TreeFolderInfo
+	Files      []*FileInfo
+	IsScanned  bool
+	CantAccess bool
+	FolderName string
+}
+
+type FileInfo struct {
+	FileName   string
+	FileSize   int64
+	FileExt    string
+	CantAccess bool
+}
+
+func NewRootItem(absolutePath string) *TreeFolderInfo {
+	return &TreeFolderInfo{
+		Parent:     nil,
+		SubFolders: make([]*TreeFolderInfo, 0),
+		Files:      make([]*FileInfo, 0),
+		IsScanned:  false,
+		FolderName: absolutePath,
+		CantAccess: false,
+	}
+}
+
+func newSubFolderItem(folderName string, parent *TreeFolderInfo) *TreeFolderInfo {
+	return &TreeFolderInfo{
+		Parent:     parent,
+		SubFolders: make([]*TreeFolderInfo, 0),
+		Files:      make([]*FileInfo, 0),
+		IsScanned:  false,
+		CantAccess: false,
+		FolderName: folderName,
+	}
+}
+
+func (f *TreeFolderInfo) GetAbsolutePath() string {
+	if f.Parent == nil {
+		return f.FolderName
+	} else {
+		return f.Parent.FolderName + "\\" + f.FolderName
+	}
+}
