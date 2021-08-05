911a502073a931524f4bb99bb3458496d853aa03
Yurij Rubakha <yurij.osbb@gmail.com>
2021-08-05 17:38:39 +0300

    RefactoredPaths

--- domain/folderTree/iTreeFilter.go
+++ domain/folderTree/iTreeFilter.go
@@ -1,6 +1,8 @@
 package treePrinter
 
-import "FileSystemTree4.mod/FileSystemTree4/domain/folderTree"
+import (
+	"FileSystemTree4/domain/folderTree"
+)
 
 type ITreeFilter interface {
 	IsFileMatchFilter (f *folderTree.FileInfo) bool
---
+++ go.mod
@@ -0,0 +1,3 @@
+module filesystemtree4
+
+go 1.16
